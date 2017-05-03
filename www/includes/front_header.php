5ac441aac7f48c07a61940d9b0b211f74caefa41
dimeji <dimeji.taiwo5@outlook.com>
2017-05-03 10:17:15 +0100

    front end begins

---
+++ www/includes/front_header.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Blog Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="styles/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="styles/blog.css" rel="stylesheet">
+  </head>
